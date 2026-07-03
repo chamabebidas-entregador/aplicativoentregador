fd39373b20867043273cc6590162dfc6275ce340
chamabebidas-entregador <121693105+chamabebidas-entregador@users.noreply.github.com>
2026-07-03 03:15:16 -0300

    Create HomeScreenNovo component with basic layout

---
+++ HomeScreenNovo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreenNovo() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.logo}>DELL
